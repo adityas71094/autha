937a04d20f6f3cfe2c84b1227f7875992d796a26
Aditya Subramanian <aditarun2008@gmail.com>
2020-01-22 02:17:12 +0530

    editUser changes
    A partial User pojo can be used to update the user record in DB.

--- src/main/java/com/adiSuper/autha/user/UserController.java
+++ src/main/java/com/adiSuper/autha/user/UserController.java
@@ -1,7 +1,10 @@
 package com.adiSuper.autha.user;
 
 
+import com.adiSuper.generated.core.Tables;
 import com.adiSuper.generated.core.tables.pojos.Users;
+import com.adiSuper.generated.core.tables.records.UsersRecord;
+import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +18,17 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private DSLContext db;
+
+    
     @GetMapping("/users")
     public List<Users> getUsers()
     {
-        //Object a = db.select(Tables.USERS.USERNAME).from(Tables.USERS).fetch();
         List<Users> users = userRepository.findAll();
         return users;
-
     }
+
     @GetMapping ("/users/{id}")
     public Users getUser(@PathVariable Integer id)
     {
@@ -41,8 +47,9 @@ public class UserController {
     public int editUser(@RequestBody Users user, @PathVariable Integer id)
     {
         Optional<Users> oldUser = userRepository.findById(id);
-        //user.setId(Math.toIntExact(id));
-        // TODO : update
+        user.setId(1);
+        UsersRecord userRecord = db.newRecord(Tables.USERS, user);
+        userRecord.update();
         return 1;
     }
 
